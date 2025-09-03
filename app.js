0f2c6db6ea1ffb3a645245f700997841b4d8767c
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-09-03 22:35:44 +0200

    Add full interactive Study Reminder App with neon-glass cards and animated background

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-const addBtn = document.querySelector('#addBtn')
-const subjectInput = document.querySelector('#subject')
-const timeInput = document.querySelector('#time')
-const remindersList = document.querySelector('#reminders')
-const errorMsg = document.querySelector('#error')
+const addBtn = document.querySelector('#addBtn');
+const subjectInput = document.querySelector('#subject');
+const timeInput = document.querySelector('#time');
+const remindersList = document.querySelector('#reminders');
+const errorMsg = document.querySelector('#error');
 
 function addReminder() {
-  const subject = subjectInput.value
-  const time = timeInput.value
+  const subject = subjectInput.value;
+  const time = timeInput.value;
 
   if (time === "") {
-    errorMsg.innerHTML = "Please enter a time"
-    return
+    errorMsg.innerHTML = "Please enter a time";
+    return;
   }
 
-  const reminderString = `${subject} at ${time}`
+  const reminderString = `${subject} at ${time}`;
 
-  const li = document.createElement('li')
-  li.textContent = reminderString
+  const li = document.createElement('li');
+  li.textContent = reminderString;
+  li.classList.add('reminder-card');
 
-  const delBtn = document.createElement('button')
-  delBtn.textContent = "Delete"
-  
+  // Delete button
+  const delBtn = document.createElement('button');
+  delBtn.textContent = "Delete";
+  delBtn.classList.add('add-btn');
+  delBtn.style.padding = '5px 10px';
+  delBtn.style.fontSize = '14px';
   delBtn.onclick = function() {
-    li.remove()
+    li.remove();
   }
 
-  li.appendChild(delBtn)
-  remindersList.appendChild(li)
+  li.appendChild(delBtn);
+  remindersList.appendChild(li);
 
-  errorMsg.innerHTML = ""
-  subjectInput.value = ""
-  timeInput.value = ""
+  errorMsg.innerHTML = "";
+  subjectInput.value = "";
+  timeInput.value = "";
 }
 
-addBtn.addEventListener('click', addReminder)
+addBtn.addEventListener('click', addReminder);
+
 
 
 
