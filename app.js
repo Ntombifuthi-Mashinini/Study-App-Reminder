901bfa5940e550348d0bbc9187dc1aa80682c00a
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-09-03 20:48:40 +0200

    Update app.js: append reminders as list items and clear inputs

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const subject = document.querySelector('#subject').value 
-const time = document.querySelector('#time').value 
+const subject = document.querySelector('#subject').value
+const time = document.querySelector('#time').value
 
-if (time === "") { 
-  document.querySelector('#error').innerHTML = "Please enter a time" 
-} else { 
+if (time === "") {
+  document.querySelector('#error').innerHTML = "Please enter a time"
+} else {
   const reminderString = `${subject} at ${time}`
-  document.querySelector('#reminders').innerHTML = reminderString 
-  document.querySelector('#error').innerHTML = "" 
+
+  const li = document.createElement('li')
+  li.textContent = reminderString
+
+  document.querySelector('#reminders').appendChild(li)
+
+  document.querySelector('#error').innerHTML = ""
+  
+  document.querySelector('#subject').value = ""
+  document.querySelector('#time').value = ""
 }
+
